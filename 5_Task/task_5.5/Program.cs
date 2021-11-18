9318ba1bf22dbb83e9e8458dd9f4cb55aee944dc
Алексей Ащеулов <ashalexx@gmail.com>
2021-11-18 17:02:35 +0700

    коментарии добавил

--- 5_Task/task_5.5/Program.cs
+++ 5_Task/task_5.5/Program.cs
@@ -10,19 +10,21 @@ namespace task_5._5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите целочисленное число: ");
             int N = Convert.ToInt32(Console.ReadLine());
 
-            int[,] A = new int[N,N];
+            int[,] A = new int[N,N];//создание 2мерного массива
 
             for (int i = 0; i < N; i++)
             {
                 for (int j = 0; j < N; j++)
                 {
-                    A[i, j] = 1 - (i + j) % 2;
-                    Console.Write(A[i,j]+" ");
+                    A[i, j] = 1 - (i + j) % 2;//деление с остатком чтобы получить 1 0 1 0
+                    Console.Write("{0} ", A[i,j]);
                 }
-                Console.WriteLine();
+                Console.WriteLine(); //перенос строки, чтобы небыло все в одну длинную строку
             }
+            Console.WriteLine();                     
             
             Console.WriteLine("Нажмите любую клавишу...");
             Console.ReadKey();
