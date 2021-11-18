ea886fe7c29e0e9d2da02d0d0d8c580c0b37e5eb
Алексей Ащеулов <ashalexx@gmail.com>
2021-11-18 15:26:27 +0700

    Написание 3 задачи 5 урока

--- 5_Task/task_5.3/Program.cs
+++ 5_Task/task_5.3/Program.cs
@@ -13,24 +13,56 @@ namespace task_5._3
             int[] numbers = new int[10];
 
             Random random = new Random();
-
-
-            for (int i = 0; i < numbers.Length; i++)
+            Console.Write("Дан рандомный массив: ");
+            for (int i = 0; i < 10; i++)
             {
                 numbers[i] = random.Next(-50, 50);
                 Console.Write("{0} ", numbers[i]);
             }
             Console.WriteLine();
 
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < 4; i++)
+			{
+                for (int j = i+1; j < 5; j++)
+			    {
+                    if (numbers[i]>numbers[j])
+	                {
+                        int t=numbers[i];
+                        numbers[i]=numbers[j];
+                        numbers[j]=t;
+	                }
+			    }
+			}
+
+            Console.WriteLine();
+
+            Console.Write("Первые 5 элементов массива по возрастанию: ");
+            for (int i = 0; i < 5; i++)
+			{
+                Console.Write("{0} ", numbers[i]);
+			}            
+            Console.WriteLine();
+            for (int i = 5; i < 9; i++)
 			{
                 for (int j = i+1; j < 10; j++)
+			    {
+                    if (numbers[i]<numbers[j])
+	                {
+                        int t=numbers[i];
+                        numbers[i]=numbers[j];
+                        numbers[j]=t;
+	                }
+			    }
+			}            
+           
+            Console.Write("Вторые 5 элементов массива по убыванию: ");
+            for (int i = 5; i < 10; i++)
 			{
-
+                Console.Write("{0} ", numbers[i]);
 			}
-			}
-
-
+            Console.WriteLine();
+            
+            Console.WriteLine();
             Console.WriteLine("Нажмите любую клавишу...");
             Console.ReadKey();
         }
