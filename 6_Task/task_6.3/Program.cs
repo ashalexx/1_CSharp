ce29c0a2e6692618461fccc7778b97b0abf6c2a3
Алексей Ащеулов <ashalexx@gmail.com>
2021-11-19 13:44:00 +0700

    не смог решить задачу 6.3

---
+++ 6_Task/task_6.3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_6._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //не смог решить задачу
+
+            string S = "{текст заключенный в фигурные скобки} текст без скобок }текс в скобках на оборот{";
+            string newS = "";
+            for (int i = 0; i < S.Length; i++)
+            {
+                newS = Convert.ToString(S[i]);
+                if (S[i]=='{')
+                {                    
+                    
+                    
+                }
+                Console.Write(newS);
+            }
+            Console.WriteLine();
+            
+            
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
