a49a6b0e2314e25dbadbbde2c262256d27d1dc79
Алексей Ащеулов <ashalexx@gmail.com>
2021-11-22 22:45:19 +0700

    вроде ,супустя часов 5, решил задачу 8.1

--- 7_Task/task_7.2/Program.cs
+++ 7_Task/task_7.2/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace task_7._2
 {
     internal class Program
---
+++ 8_Task/task_8.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace task_8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string dirName="D:\\test";
+ 
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("=============Подкаталоги=============\n");
+                string[] dirs = Directory.GetDirectories(dirName, "*", SearchOption.AllDirectories);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);                   
+
+                }
+                Console.WriteLine();
+                Console.WriteLine("=============Файлы=============\n");
+                 
+                string[] files = Directory.GetFiles(dirName, "*", SearchOption.AllDirectories);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+
+        }
+    }
+}
