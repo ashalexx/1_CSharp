aeb4dc4824247695165bc0d9e3dfc3bd526866ef
Алексей Ащеулов <ashalexx@gmail.com>
2021-11-26 14:44:27 +0700

    еще чуть чуть и готова задача 10.1

--- 10_Task/task_10.1/Program.cs
+++ 10_Task/task_10.1/Program.cs
@@ -5,6 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+
+//Person tom = new Person();  // создание объекта класса Person
+
+//// определение класса Person
+//class Person
+//{
+//    public string name = "Undefined";
+//    public int age;
+
+//    public void Print()
+//    {
+//        Console.WriteLine($"Имя: {name}  Возраст: {age}");
+//    }
+//}
 namespace task_10._1
 {
     internal class Program
@@ -13,45 +27,87 @@ namespace task_10._1
         {
             
             MyClass qw = new MyClass();
-            qw.gradus = -8;
-            qw.min = 1;
-            qw.sec = 45;            
-            qw.ToRadians();         
+            qw.Gradus = 8;//не забываем про БОЛЬШУЮ заглавную букву
+            qw.Min = 1;
+            qw.Sec = 45;
+            
+            qw.ToRadians();
+            Console.WriteLine();
                               
             Console.WriteLine("Нажмите любую клавишу...");
             Console.ReadKey();
-        }              
-	}
+        }
+    }
     public class MyClass
-	    {
-            public int gradus;
-            public int min { get; set; }
-            public int sec { get; set; }
-
+    {
+        private int gradus;
+        private int min;
+        private int sec;
+                
         public int Gradus
+        {
+            set
             {
-                set 
-                { 
-                    if (value>0)
-	                {
-                        gradus = value;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Значение gradus отрицательно");
-                    }	
+                if (value > 0)
+                {
+                    gradus = value;
                 }
-                get 
-                { 
-                    return gradus; 
+                else
+                {
+                    Console.WriteLine("Значение gradus отрицательное\n");
                 }
             }
-
-            public void ToRadians()
+            get
             {
-                int Radians = Convert.ToInt32((gradus + min / 60 + sec / 3600) * (Math.PI) / 180);
-                Console.WriteLine(Radians);
+                return gradus;
             }
         }
+        public int Min
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    min = value;
+                }
+                else
+                {
+                    Console.WriteLine("Значение min отрицательное\n");
+                }
+            }
+            get
+            {
+                return min;
+            }
+        }
+        public int Sec
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    sec = value;
+                }
+                else
+                {
+                    Console.WriteLine("Значение sec отрицательное\n");
+                }
+            }
+            get
+            {
+                return sec;
+            }
+        }
+
+        public void ToRadians()
+            {
+                if (true)
+                {
+
+                }
+                double Radians = Convert.ToDouble((gradus + min / 60 + sec / 3600) * (Math.PI) / 180);
+                Console.WriteLine("{0} Радиан", Radians);
+            }
+    }
 	    
 }
