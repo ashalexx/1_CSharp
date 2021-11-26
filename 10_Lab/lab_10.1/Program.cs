7438698dd0942547e521f458c3e0511e70a54f56
Алексей Ащеулов <ashalexx@gmail.com>
2021-11-26 08:58:05 +0700

    Задача 10.1 , разобраться

---
+++ 10_Lab/lab_10.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Мурка", "синяя", 23); //создали экземпляр класса
+        }
+    }
+    class Cat
+    {
+        string name; //поля
+        string color;
+        int age;
+
+        Cat(string n, string c, int a) //конструктор
+        {
+            name = n;
+            color = c;
+            age = a;
+        }
+
+        void Say()// методы
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
---
+++ 10_Task/task_10.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task_10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyClass qw = new MyClass();
+            qw.gradus = -8;
+            qw.min = 1;
+            qw.sec = 45;            
+            qw.ToRadians();         
+                              
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }              
+	}
+    public class MyClass
+	    {
+            public int gradus;
+            public int min { get; set; }
+            public int sec { get; set; }
+
+        public int Gradus
+            {
+                set 
+                { 
+                    if (value>0)
+	                {
+                        gradus = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Значение gradus отрицательно");
+                    }	
+                }
+                get 
+                { 
+                    return gradus; 
+                }
+            }
+
+            public void ToRadians()
+            {
+                int Radians = Convert.ToInt32((gradus + min / 60 + sec / 3600) * (Math.PI) / 180);
+                Console.WriteLine(Radians);
+            }
+        }
+	    
+}
