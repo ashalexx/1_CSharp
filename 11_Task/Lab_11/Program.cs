eaafcc6fe539de0c4a86ecc23da6524a9c6f8c69
Алексей Ащеулов <ashalexx@gmail.com>
2021-11-27 17:11:35 +0700

    ох и тяжело идет ООП решение задачи 11

---
+++ 11_Task/Lab_11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using bibl;
+
+
+//Person tom = new Person();  // создание объекта класса Person
+
+//// определение класса Person
+//class Person
+//{
+//    public string name = "Undefined";
+//    public int age;
+
+//    public void Print()
+//    {
+//        Console.WriteLine($"Имя: {name}  Возраст: {age}");
+//    }
+//}
+namespace task_10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass qw = new MyClass(8, 0, 0);//создаем экземпляр класса 
+                                              //qw.Gradus = 8;//не забываем про БОЛЬШУЮ заглавную букву
+                                              //qw.Min = 0;
+                                              //qw.Sec = 0;
+
+            qw.ToRadians();//вызов метода класса MyClass
+            Console.WriteLine();
+
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }   
+
+}
---
+++ 11_Task/task_11.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStruct my = new MyStruct { b = 1, k = 4 };
+            my.Root();
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+        struct MyStruct
+        {
+            public double k;
+            public double b;
+            public double x;
+            public void Root()
+            {
+                x = (0 - b) / k;
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
