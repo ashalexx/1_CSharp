0c651c2d2244c309c5f8580c0477f7fb9d2d62dc
Алексей Ащеулов <ashalexx@gmail.com>
2021-12-01 17:50:52 +0700

    не понятно, разберусь

---
+++ 16_Task/lab_16/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+
+namespace lab_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = "{\"firstName\":\"Tom\",\"lastName\":\"Jackson\",\"gender\":\"male\",\"age\":29,\"online\":true,\"hobby\":[\"footbal\",\"reading\", \"swiming\"]}"; // СТРОКА
+            Person person = JsonSerializer.Deserialize<Person>(jsonString);
+            Person person1 = new Person()
+            {
+                FirstName = "Вася",
+                LastName = "Пупкин",
+                Gender = "муж",
+                Age = 24,
+                Online = true,
+                Hobby = new string[] { "Футбол", "программирование" }
+            };
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string jsonString1 = JsonSerializer.Serialize(person1, options);
+
+            Console.WriteLine(jsonString1);
+
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+        class Person
+        {
+            [JsonPropertyName("firstName")]
+            public string FirstName { get; set; }
+            [JsonPropertyName("lastName")]
+            public string LastName { get; set; }
+            [JsonPropertyName("gender")]
+            public string Gender { get; set; }
+            [JsonPropertyName("age")]
+            public int Age { get; set; }
+            [JsonPropertyName("online")]
+            public bool Online { get; set; }
+            //[JsonPropertyName("hobby")]
+            [JsonIgnore]
+            public string[] Hobby { get; set; }
+
+
+        }
+    }
+}
---
+++ 16_Task/task_16.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_16._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product(1, "Телевизор", 45);
+            
+            Product[] product_arr = new Product[5];
+            product_arr[0] = product; //это я пробовал заосить экземпляр класса в массив 
+            
+            
+
+            Console.WriteLine(product_arr[1]);
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        public int productCode { get; set; }
+        public string productName { get; set; }
+        public double productPrice { get; set; }
+
+        public Product(int productCode, string productName, double productPrise)
+        {
+            this.productCode = productCode;
+            this.productName = productName;
+            this.productPrice = productPrise;
+        }        
+    }
+}
