be628272eb2d268ace4062c42bd2b0db0c7cfa85
Алексей Ащеулов <ashalexx@gmail.com>
2021-12-09 13:06:30 +0700

    й

---
+++ 18_Task/lab_18/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            int[] vs = new int[] { 1, 2, 3 };
+            list.AddRange(vs);
+            list.RemoveAt(3);
+            list.Reverse();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i]++;
+            }
+
+            foreach (int l in list)
+            {
+                Console.WriteLine(l);
+            }
+            Console.WriteLine("Нажмте любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 18_Task/task_18.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_18._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "([al(s)kdv{lam}v{]})";
+            
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char item in str)
+            {                
+                switch (item)
+                {
+                    case '(': stack.Push(')'); break;
+                    case '[': stack.Push(']'); break;
+                    case '{': stack.Push('}'); break;
+
+                    //case ')':
+                    //case ']':
+                    //case '}':
+                    //    if (stack.Count == 0)
+                    //    {
+                    //        Console.WriteLine("Дана строка {0}\n", str);
+                    //        Console.WriteLine("Расположение скобок не правильное.");
+                    //    }
+                    //    stack.Pop();
+                    //    break;
+                    //default:
+                    //    break;
+                }
+            }
+            
+
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
