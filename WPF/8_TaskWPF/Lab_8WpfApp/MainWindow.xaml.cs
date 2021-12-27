9ea9c59baf2add6a3d3d1234d68f78e6242e3420
Алексей Ащеулов <ashalexx@gmail.com>
2021-12-27 21:30:50 +0700

    done

--- WPF/8_TaskWPF/Lab_8WpfApp/MainWindow.xaml.cs
+++ WPF/8_TaskWPF/Lab_8WpfApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,98 @@ namespace Lab_8WpfApp
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //string fountName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            //if (textBox != null)
+            //{
+            //    textBox.FontFamily = new FontFamily(fountName);
+            //}
+
+            string fountName = (sender as ComboBox).SelectedItem as string;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fountName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string fount = (sender as ComboBox).SelectedItem as string;
+            if (textBox != null)
+            {
+                textBox.FontSize = Convert.ToDouble(fount);
+            }
+        }
+        private void BExecuted(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+                textBox.FontWeight = FontWeights.Bold;
+            else
+                textBox.FontWeight = FontWeights.Normal;
+        }
+
+        private void IExecuted(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+                textBox.FontStyle = FontStyles.Italic;
+            else
+                textBox.FontStyle = FontStyles.Normal;
+        }
+
+        private void UExecuted(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+                if (textBox.TextDecorations != TextDecorations.Baseline)
+                    textBox.TextDecorations = TextDecorations.Baseline; // не пойму почему не сробатывает кнопка с первого нажатия
+                else
+                    textBox.TextDecorations = null;
+        }
+        private void Foreground1Executed(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+                //if (true)
+                textBox.Foreground = new SolidColorBrush(Colors.Black);
+            //if(false)
+            //    textBox.Foreground = null;
+        }
+
+        private void Foreground2Executed(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+                //    if (true)
+                textBox.Foreground = new SolidColorBrush(Colors.Red);
+            //if (false)
+            //    textBox.Foreground = null;
+        }
+
+        private void OpenExecuted(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовый файл(*.txt)|*.txt|Все файлы(*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void SaveExecuted(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовый файл(*.txt)|*.txt|Все файлы(*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+        }
+
+        private void ExitExecuted(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ WPF/8_TaskWPF/Lab_8WpfApp/MyCommands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab_8WpfApp
+{
+    internal class MyCommands
+    {
+        public static RoutedCommand B { get; set; }
+        public static RoutedCommand I { get; set; }
+        public static RoutedCommand U { get; set; }
+        public static RoutedCommand Foreground1 { get; set; }
+        public static RoutedCommand Foreground2 { get; set; }
+        
+        static MyCommands()
+        {
+            B = new RoutedCommand();
+            I = new RoutedCommand();
+            U = new RoutedCommand();
+            Foreground1 = new RoutedCommand();
+            Foreground2 = new RoutedCommand();
+            
+        }       
+
+    }
+}
