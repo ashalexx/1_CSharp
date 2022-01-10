9a923ec8bf7c77358b1ca59ea8af486b84a1bd92
Алексей Ащеулов <ashalexx@gmail.com>
2022-01-10 13:09:16 +0700

    done

---
+++ WPF/14_TaskWPF/task_14WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task_14WpfApp
+{
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProductType = ProductTypes.Food,
+                Name = "Хлеб",
+                Image = 1,
+                Price = 40
+            });
+            products.Add(new Product()
+            {
+                ProductType = ProductTypes.Food,
+                Name = "Молоко",
+                Image = 2,
+                Price = 70
+            });
+            products.Add(new Product()
+            {
+                ProductType = ProductTypes.Appliances,
+                Name = "Утюг",
+                Image = 3,
+                Price = 1234
+            });
+            lstBox.ItemsSource = products;
+        }
+
+        
+    }
+}
---
+++ WPF/14_TaskWPF/task_14WpfApp/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_14WpfApp
+{
+    public enum ProductTypes
+    {
+        Food,
+        Appliances
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Image { get; set; }
+        public ProductTypes ProductType { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+
+}
