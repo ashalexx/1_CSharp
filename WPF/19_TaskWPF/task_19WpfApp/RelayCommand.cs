5b6f25f7b975917381d33db337c3cf24a38bb4c5
Алексей Ащеулов <ashalexx@gmail.com>
2022-01-14 17:53:08 +0700

    done

---
+++ WPF/19_TaskWPF/task_19WpfApp/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace task_19WpfApp
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged // сробатывает каждый раз когда CanExecute изменилось
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        // создаем конструктор
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true; // проверяет доступность комманды
+        
+        public void Execute(object parameter) => execute(parameter); // метод выполнится когда комманда будет вызвана
+
+    }
+}
